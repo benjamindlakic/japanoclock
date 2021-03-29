956c98a8440a9b3b9be2fc56b40e9a1b1ac3905b
Benjamin Dlakic <81329573+benjamindlakic@users.noreply.github.com>
2021-03-29 22:33:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ var updateClock = function()
 {
   var time = new Date().getHours();
   var messageText;
-  var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
+  var image = "spavanac.jpg";
 
   var timeEventJS = document.getElementById("timeEvent");
   var lolcatImageJS = document.getElementById('lolcatImage');
